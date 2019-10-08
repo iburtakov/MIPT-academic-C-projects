41009240fb08face50abe8b7cea0aba8b170a1ac
Teofelts <43933467+Teofelts@users.noreply.github.com>
2019-10-09 00:51:31 +0300

    First implementation
    It can copy, sort and sort from end of string

---
+++ Encyclopedia/Sort.h
@@ -0,0 +1,18 @@
+//
+// Created by Илья on 08.10.2019.
+//
+
+#ifndef ENCYCLOPEDIA_SORT_H
+#define ENCYCLOPEDIA_SORT_H
+
+#include <ctype.h>
+#include <string.h>
+
+int strLength (char *str);
+
+int strCompare (const void *lineOne, const void *lineTwo);
+
+int strCompareFromEnd (const void *lineOne, const void *lineTwo);
+
+
+#endif //ENCYCLOPEDIA_SORT_H
